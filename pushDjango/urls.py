2eaf0b4d23047f2e7f0164051942fbc4e4713867
ashish1sasmal <ashishsasmal1@gmail.com>
2020-05-16 12:54:06 +0530

    sec

--- pushDjango/urls.py
+++ pushDjango/urls.py
@@ -17,8 +17,11 @@ from django.contrib import admin
 from django.urls import path
 from push import views
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home,name='home'),
     path('login/',views.user_login,name='user_login'),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
